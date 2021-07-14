584796886dde9afa0c2fdb0999e541c980b17dea
Levon Karamyan <lyov.karamyan@gmail.com>
2021-07-15 02:37:51 +0400

    change environment

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -23,7 +23,7 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment() === 'production') {
+        if (!$request->secure() && app()->environment() !== 'local') {
             return redirect()->secure($request->getRequestUri());
         }
 
